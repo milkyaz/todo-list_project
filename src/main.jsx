315730630a9b7417ab1b5f20965a461ac2c505ae
Daniil Zaburunov <zaburunov.2000@mail.ru>
2024-09-30 21:10:38 +0400

    добавил vite

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import App from './App'
 import { store } from "./store/index";
 import { Provider } from "react-redux";
 import { createBrowserRouter } from "react-router-dom";
